f6716a62ba918684557b6a35fd4f5f1e67ed4c7f
jeancmr <jean.madiedo08@gmail.com>
2023-02-07 20:21:51 -0500

    finishedd

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Header from "./components/Header";
 import Card from "./components/Card";
+import placesData from "./data";
+
 function App() {
+  const cardElements = placesData.map((card) => (
+    <Card key={card.id} {...card} />
+  ));
+
   return (
     <main className="container">
       <Header />
-      <section className="section-cards">
-        <Card />
-      </section>
+      <section className="section-cards">{cardElements}</section>
     </main>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,25 +3,19 @@ import gps from "../assets/gps.svg";
 export default function Card(props) {
   return (
     <div className="card">
-      <img
-        className="card-img"
-        src="https://source.unsplash.com/WLxQvbMyfas"
-        alt="frontier"
-      />
+      <img className="card-img" src={props.imageUrl} alt="frontier" />
 
       <div className="text-box">
         <div className="tag">
           <img className="gps-icon" src={gps} alt="gps logo" />
-          <p className="country">japan</p>
-          <a href="#">view on google maps</a>
+          <p className="country">{props.location}</p>
+          <a href={props.googleMapsUrl}>view on google maps</a>
         </div>
-        <h2 className="card-place">Mount Fuji</h2>
-        <span className="card-dates">12 Jan, 2021 - 24 Jan, 2021</span>
-        <p className="card-paragraph">
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
+        <h2 className="card-place">{props.title}</h2>
+        <p className="card-dates">
+          <span>{props.startDate}</span> - <span>{props.endDate}</span>
         </p>
+        <p className="card-paragraph">{props.description}</p>
       </div>
     </div>
   );
